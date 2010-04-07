e417823a511bffbab9fffc916502191988a2a97d
Benjamin Doherty <bjd@pobox.com>
2010-04-07 08:49:49 -0500

    detect and change directionality of textareas based on contents.

--- bidi.rtlButtons.js
+++ bidi.rtlButtons.js
@@ -3,4 +3,7 @@ goog.require('goog.i18n.bidi');
 Drupal.behaviors.bidiRtlButtons = function (context) {
   $body = $('#edit-body-wrapper', context);
 
+  $('textarea', $body).each(function () {
+    goog.i18n.bidi.setElementDirAndAlign(this, goog.i18n.bidi.detectRtlDirectionality($(this).val()));
+  });
 };
